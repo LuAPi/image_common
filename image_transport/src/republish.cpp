62ef865e4022bc0127fc328816180c667e217072
Luke Pitt <luke@robots.ox.ac.uk>
2021-04-14 16:48:04 +0100

    Initial lazy subscription implementation for image_transport republish

--- image_transport/src/republish.cpp
+++ image_transport/src/republish.cpp
@@ -35,6 +35,10 @@
 #include "image_transport/image_transport.h"
 #include "image_transport/publisher_plugin.h"
 #include <pluginlib/class_loader.h>
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <mutex>
 
 int main(int argc, char** argv)
 {
@@ -47,18 +51,43 @@ int main(int argc, char** argv)
   std::string in_topic  = nh.resolveName("in");
   std::string in_transport = argv[1];
   std::string out_topic = nh.resolveName("out");
+  bool lazy;
+  nh.getParam("lazy", lazy);
+  std::atomic_bool subscribed(false);
+  std::mutex subscription_mutex;
 
   image_transport::ImageTransport it(nh);
   image_transport::Subscriber sub;
   
   if (argc < 3) {
     // Use all available transports for output
-    image_transport::Publisher pub = it.advertise(out_topic, 1);
+    image_transport::Publisher pub;
     
-    // Use Publisher::publish as the subscriber callback
-    typedef void (image_transport::Publisher::*PublishMemFn)(const sensor_msgs::ImageConstPtr&) const;
-    PublishMemFn pub_mem_fn = &image_transport::Publisher::publish;
-    sub = it.subscribe(in_topic, 1, boost::bind(pub_mem_fn, &pub, _1), ros::VoidPtr(), in_transport);
+    std::function<void(void)> on_new_subscriber([&lazy, &subscribed, &sub, &pub, &it, &in_topic, &in_transport, &subscription_mutex](){
+        std::unique_lock<std::mutex> lk(subscription_mutex);
+        if(!subscribed.load()){
+          // Use Publisher::publish as the subscriber callback
+          typedef void (image_transport::Publisher::*PublishMemFn)(const sensor_msgs::ImageConstPtr&) const;
+          PublishMemFn pub_mem_fn = &image_transport::Publisher::publish;
+          sub = it.subscribe(in_topic, 1, boost::bind(pub_mem_fn, &pub, _1), ros::VoidPtr(), in_transport);
+          subscribed = true;
+        }
+      });
+    image_transport::SubscriberStatusCallback on_subscriber_left(
+      [&lazy, &subscribed, &sub, &pub, &it, &subscription_mutex](const image_transport::SingleSubscriberPublisher& ssp){
+        std::unique_lock<std::mutex> lk(subscription_mutex);
+        if(lazy && pub.getNumSubscribers() == 0){
+          sub.shutdown();
+          subscribed = false;
+        }
+      }
+    );
+
+    pub = it.advertise(out_topic, 1, boost::bind(on_new_subscriber), on_subscriber_left);
+    if(!lazy){
+      on_new_subscriber();
+    }
+    
 
     ros::spin();
   }
@@ -71,13 +100,35 @@ int main(int argc, char** argv)
     pluginlib::ClassLoader<Plugin> loader("image_transport", "image_transport::PublisherPlugin");
     std::string lookup_name = Plugin::getLookupName(out_transport);
     boost::shared_ptr<Plugin> pub( loader.createInstance(lookup_name) );
-    pub->advertise(nh, out_topic, 1, image_transport::SubscriberStatusCallback(),
-                   image_transport::SubscriberStatusCallback(), ros::VoidPtr(), false);
 
-    // Use PublisherPlugin::publish as the subscriber callback
-    typedef void (Plugin::*PublishMemFn)(const sensor_msgs::ImageConstPtr&) const;
-    PublishMemFn pub_mem_fn = &Plugin::publish;
-    sub = it.subscribe(in_topic, 1, boost::bind(pub_mem_fn, pub.get(), _1), pub, in_transport);
+    std::function<void(void)> on_new_subscriber(
+      [&lazy, &subscribed, &sub, &pub, &it, &in_topic, &in_transport, &subscription_mutex](){
+        std::unique_lock<std::mutex> lk(subscription_mutex);
+        if(!subscribed.load()){
+          // Use PublisherPlugin::publish as the subscriber callback
+          typedef void (Plugin::*PublishMemFn)(const sensor_msgs::ImageConstPtr&) const;
+          PublishMemFn pub_mem_fn = &Plugin::publish;
+          sub = it.subscribe(in_topic, 1, boost::bind(pub_mem_fn, pub.get(), _1), pub, in_transport);
+          subscribed = true;
+        }
+      }
+    );
+    image_transport::SubscriberStatusCallback on_subscriber_left(
+      [&lazy, &subscribed, &sub, &pub, &it, &subscription_mutex](const image_transport::SingleSubscriberPublisher& ssp){
+        std::unique_lock<std::mutex> lk(subscription_mutex);
+        if(lazy && pub->getNumSubscribers() == 0){
+          sub.shutdown();
+          subscribed = false;
+        }
+      }
+    );
+
+    pub->advertise(nh, out_topic, 1, boost::bind(on_new_subscriber),
+                   on_subscriber_left, ros::VoidPtr(), false);
+
+    if(!lazy){
+      on_new_subscriber();
+    }
 
     ros::spin();
   }
